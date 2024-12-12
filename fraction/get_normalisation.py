298f91b3f400811605205dd4eddd17d4ee21e3e3
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2024-12-12 20:40:33 +0100

    Add efficiency of Zvtx cut in normalisation

--- fraction/get_normalisation.py
+++ fraction/get_normalisation.py
@@ -15,9 +15,14 @@ def get_lumi(infile_names, suffix):
     lumi = 0
     for infile_name in infile_names:
         infile = ROOT.TFile.Open(infile_name)
+        hist_collisions = infile.Get("hf-candidate-creator-dstar/hCollisions")
+        if not isinstance(hist_collisions, ROOT.TH1):
+            hist_collisions = infile.Get("hf-candidate-creator-3-prong/hCollisions")
+        n_bins = hist_collisions.GetNbinsX()
+        zvtx_eff = hist_collisions.GetBinContent(n_bins) / hist_collisions.GetBinContent(n_bins-1)
         hist_lumi_per_run.append(infile.Get("bc-selection-task/hLumiTVXafterBCcuts"))
         for ibin in range(1, hist_lumi_per_run[-1].GetNbinsX()+1):
-            lumi += hist_lumi_per_run[-1].GetBinContent(ibin) # mub-1
+            lumi += hist_lumi_per_run[-1].GetBinContent(ibin) * zvtx_eff # mub-1
 
     hist_lumi = ROOT.TH1F("hist_lumi", ";;luminosity (pb^{#minus1})", 1, 0., 1.)
     hist_lumi.SetBinContent(1, lumi)
